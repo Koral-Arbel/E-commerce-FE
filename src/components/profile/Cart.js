0bd87be125195befed317b2a2e432b3df8c68b52
Koral <korali1995@gmail.com>
2024-01-26 04:12:58 +0200

    table orders close work

--- src/components/profile/Cart.js
+++ src/components/profile/Cart.js
@@ -24,9 +24,22 @@ function Cart() {
   const { userDetails } = useContext(UserProfileContext);
   const { cart, setCart } = useContext(CartContext);
   const { orders, setOrders } = useContext(OrdersContext);
-  const [selectedOrder, setSelectedOrder] = useState(null);
+  const [orderDetails, setOrderDetails] = useState({
+    userId: userDetails.id,
+    orderDate: null,
+    shippingAddress: "",
+    status: "TEMP",
+    orderNumber: null,
+    items: [], // Initialize items as an empty array
+  });
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  const splitOrdersByStatus = (orders) => {
+    const tempOrders = orders.filter((order) => order.status === "TEMP");
+    const closeOrders = orders.filter((order) => order.status === "CLOSE");
+    return [tempOrders, closeOrders];
+  };
 
   const handlerLoadCart = async () => {
     try {
@@ -41,8 +54,27 @@ function Cart() {
         const cartItems = response.data.map((order) => order.item).flat();
         setCart(cartItems);
 
-        setLoading(false);
+        const orderDetailsData = response.data[0].order || {};
+        const items = cartItems.map((cartItem) => ({
+          id: cartItem.id,
+          title: cartItem.title,
+          photo: cartItem.photo,
+          price: cartItem.price,
+          availableStock: cartItem.availableStock,
+          quantity: cartItem.quantity,
+        }));
+
+        setOrderDetails({
+          userId: userDetails.id,
+          orderDate: orderDetailsData.orderDate,
+          shippingAddress: orderDetailsData.shippingAddress,
+          status: orderDetailsData.status,
+          orderNumber: orderDetailsData.id,
+          items: items,
+        });
       }
+
+      setLoading(false);
     } catch (error) {
       console.error("Error fetching cart details:", error);
       setError("Error fetching cart details. Please try again later.");
@@ -54,100 +86,141 @@ function Cart() {
     handlerLoadCart();
   }, [auth.token, userDetails.id, setCart]);
 
-  const handleOrderClick = (order) => {
-    setSelectedOrder(order);
+  const handlerCheckout = async () => {
+    try {
+      if (orderDetails.orderNumber && cart.length > 0 && auth.token) {
+        await checkOutOrder(orderDetails.orderNumber, {}, auth.token);
+
+        if (orderDetails.status === "TEMP") {
+          setOrders((prevOrders) => [
+            {
+              orderNumber: orderDetails.orderNumber,
+              orderDate: new Date().toISOString(),
+              status: "CLOSE",
+              items: orderDetails.items,
+            },
+            ...prevOrders,
+          ]);
+        }
+
+        setOrderDetails((prevOrder) => ({
+          ...prevOrder,
+          status: "CLOSE",
+          orderDate: new Date().toISOString(),
+        }));
+
+        setCart([]);
+      }
+    } catch (error) {
+      console.error("Error during checkout:", error);
+    }
   };
 
-  const handleCloseOrder = () => {
-    setSelectedOrder(null);
-  };
+  function calculateTotalPrice(orderItems) {
+    return orderItems.reduce((total, item) => {
+      const itemPrice = item.price || 0;
+      return total + itemPrice;
+    }, 0);
+  }
 
   return (
-    <div style={{ textAlign: "center", margin: "20px 0" }}>
+    <div>
       <h1>My Cart</h1>
 
       {loading && <p>Loading...</p>}
       {error && <p style={{ color: "red" }}>{error}</p>}
+      {orderDetails.orderNumber && (
+        <>
+          <h2>Order Details</h2>
+          <p>Order Number: {orderDetails.orderNumber}</p>
+          <p>Order Date: {new Date(orderDetails.orderDate).toLocaleString()}</p>
+          <p>Shipping Address: {orderDetails.shippingAddress}</p>
+          <p>Status: {orderDetails.status}</p>
 
-      {/* Render detailed information for the selected order */}
-      {selectedOrder && (
-        <div>
-          <Typography
-            variant="h6"
-            style={{ fontSize: "1.5em", fontWeight: "bold" }}
-          >
-            Selected Order Details
-          </Typography>
-          <p>Order Number: {selectedOrder.orderNumber}</p>
-          <p>
-            Order Date: {new Date(selectedOrder.orderDate).toLocaleString()}
-          </p>
-          {/* Display additional information as needed */}
-          <TableContainer component={Paper} className="closedOrdersTable">
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Item</TableCell>
-                  <TableCell>Quantity</TableCell>
-                  <TableCell>Price</TableCell>
-                  <TableCell>Photo</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {selectedOrder.items.map((orderItem) => (
-                  <TableRow key={orderItem.id}>
-                    <TableCell>{orderItem.title}</TableCell>
-                    <TableCell>{orderItem.quantity}</TableCell>
-                    <TableCell>${orderItem.price}</TableCell>
-                    <TableCell>
-                      <img
-                        src={orderItem.photo}
-                        alt={orderItem.title}
-                        style={{ width: "50px", height: "50px" }}
-                      />
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleCloseOrder}
-          >
-            Close Order
-          </Button>
-        </div>
-      )}
-
-      {/* Render a list of closed orders */}
-      {orders.length > 0 && (
-        <div className="closedOrdersContainer">
-          <Typography
-            variant="h6"
-            style={{ fontSize: "1.5em", fontWeight: "bold" }}
-          >
-            Closed Orders
-          </Typography>
-          {orders.map((order) => (
-            <div
-              key={order.orderNumber}
-              style={{
-                margin: "20px 0",
-                cursor: "pointer",
-                padding: "10px",
-                border: "1px solid #ddd",
-                borderRadius: "5px",
-              }}
-              onClick={() => handleOrderClick(order)}
-            >
-              <h3>Order Number: {order.orderNumber}</h3>
-              <p>Order Date: {new Date(order.orderDate).toLocaleString()}</p>
-              {/* Additional information can be displayed here if needed */}
+          {/* Render the current "TEMP" status order at the top */}
+          {orderDetails.status === "TEMP" && (
+            <div id="cartContainer">
+              <Typography variant="h6">Items in Cart</Typography>
+              {cart.length > 0 ? (
+                <TableContainer component={Paper}>
+                  <Table>
+                    <TableBody>
+                      <TableRow>
+                        {cart.map((item) => (
+                          <TableCell key={item.id}>
+                            <CartItem item={item} />
+                          </TableCell>
+                        ))}
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              ) : (
+                <Typography variant="body2">Your cart is empty.</Typography>
+              )}
+              <Button
+                variant="contained"
+                color="primary"
+                className="checkoutButton"
+                onClick={handlerCheckout}
+              >
+                Checkout
+              </Button>
             </div>
-          ))}
-        </div>
+          )}
+          {/* Render a table for closed orders */}
+          {orders.length > 0 && (
+            <div className="closedOrdersContainer">
+              <Typography variant="h6">Closed Orders</Typography>
+              <TableContainer component={Paper} className="closedOrdersTable">
+                <Table>
+                  <TableBody>
+                    {orders.map((order) => (
+                      <React.Fragment key={order.orderNumber}>
+                        <TableRow>
+                          <TableCell>Order Number</TableCell>
+                          <TableCell>{order.orderNumber}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>Order Date</TableCell>
+                          <TableCell>
+                            {new Date(order.orderDate).toLocaleString()}
+                          </TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>Status</TableCell>
+                          <TableCell>{order.status}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>Total Price</TableCell>
+                          <TableCell>
+                            ${calculateTotalPrice(order.items)}
+                          </TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>Items Purchased</TableCell>
+                          <TableCell>
+                            <Table>
+                              <TableBody>
+                                <TableRow>
+                                  {order.items.map((orderItem) => (
+                                    <TableCell key={orderItem.id}>
+                                      <CartItem item={orderItem} />
+                                    </TableCell>
+                                  ))}
+                                </TableRow>
+                              </TableBody>
+                            </Table>
+                          </TableCell>
+                        </TableRow>
+                      </React.Fragment>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
