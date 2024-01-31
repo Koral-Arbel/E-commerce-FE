21dcc09eb2c51211ee5aa58f83ee8066e076fa60
Koral <korali1995@gmail.com>
2024-01-31 23:11:42 +0200

    Search bar effect

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,4 @@
-// SearchBar.js
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   List,
   ListItem,
@@ -11,6 +10,7 @@ import {
   Dialog,
   DialogTitle,
   DialogContent,
+  Button,
 } from "@mui/material";
 import { searchTerm } from "../services/api";
 
@@ -18,12 +18,21 @@ function SearchBar({ updateResults }) {
   const [searchItems, setSearchItems] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [selectedItem, setSelectedItem] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleInputChange = (event) => {
-    setSearchItems(event.target.value);
+  const debounce = (fn, delay) => {
+    let timerId;
+    return function (...args) {
+      if (timerId) {
+        clearTimeout(timerId);
+      }
+      timerId = setTimeout(() => {
+        fn(...args);
+      }, delay);
+    };
   };
 
-  const handleSearch = async () => {
+  const debouncedSearch = debounce(async () => {
     try {
       const results = await searchTerm(searchItems);
       setSearchResults(results);
@@ -32,14 +41,31 @@ function SearchBar({ updateResults }) {
       console.error("Error handling search:", error);
       setSearchResults([]);
     }
+  }, 300);
+
+  useEffect(() => {
+    if (searchItems.trim() !== "") {
+      debouncedSearch();
+    } else {
+      setSearchResults([]);
+      updateResults([]);
+    }
+  }, [searchItems, debouncedSearch, updateResults]);
+
+  const handleInputChange = (event) => {
+    setSearchItems(event.target.value);
   };
 
   const handleOpenModal = (item) => {
     setSelectedItem(item);
+    setSearchItems("");
+    setIsModalOpen(true);
   };
 
   const handleCloseModal = () => {
     setSelectedItem(null);
+    setSearchItems("");
+    setIsModalOpen(false);
   };
 
   return (
@@ -50,47 +76,48 @@ function SearchBar({ updateResults }) {
         value={searchItems}
         onChange={handleInputChange}
       />
-      <button onClick={handleSearch}>Search</button>
 
-      <Paper elevation={3} style={{ marginTop: "10px", padding: "10px" }}>
-        <List>
-          {searchResults.length > 0 ? (
-            searchResults.map((result) => (
-              <ListItem
-                key={result.id}
-                onClick={() => handleOpenModal(result)}
-                style={{ cursor: "pointer" }}
-              >
-                <ListItemAvatar>
-                  <Avatar src={result.photo} alt={result.title} />
-                </ListItemAvatar>
-                <ListItemText
-                  primary={result.title}
-                  secondary={`Price: $${result.price}`}
-                />
-              </ListItem>
-            ))
-          ) : (
-            <Typography variant="body1">No results found</Typography>
-          )}
-        </List>
-        <Dialog open={selectedItem !== null} onClose={handleCloseModal}>
-          {selectedItem && (
-            <>
-              <DialogTitle>{selectedItem.title}</DialogTitle>
-              <DialogContent>
-                <Typography>Price: ${selectedItem.price}</Typography>
-                <Typography>
-                  Photo:{" "}
-                  <img src={selectedItem.photo} alt={selectedItem.title} />
-                </Typography>
-                <Typography>Quantity: {selectedItem.quantity}</Typography>
-                {/* כאן יכניס פרטים נוספים של הפריט */}
-              </DialogContent>
-            </>
-          )}
-        </Dialog>
-      </Paper>
+      {searchItems.trim() !== "" && (
+        <Paper elevation={3} style={{ marginTop: "10px", padding: "10px" }}>
+          <List>
+            {searchResults.length > 0 ? (
+              searchResults.map((result) => (
+                <ListItem
+                  key={result.id}
+                  onClick={() => handleOpenModal(result)}
+                  style={{ cursor: "pointer" }}
+                >
+                  <ListItemAvatar>
+                    <Avatar src={result.photo} alt={result.title} />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={result.title}
+                    secondary={`Price: $${result.price}`}
+                  />
+                </ListItem>
+              ))
+            ) : (
+              <Typography variant="body1">No results found</Typography>
+            )}
+          </List>
+          <Dialog open={selectedItem !== null} onClose={handleCloseModal}>
+            {selectedItem && (
+              <>
+                <DialogTitle>{selectedItem.title}</DialogTitle>
+                <DialogContent>
+                  <Typography>Price: ${selectedItem.price}</Typography>
+                  <Typography>
+                    Photo:{" "}
+                    <img src={selectedItem.photo} alt={selectedItem.title} />
+                  </Typography>
+                  <Typography>Quantity: {selectedItem.quantity}</Typography>
+                </DialogContent>
+                <Button onClick={handleCloseModal}>Close</Button>
+              </>
+            )}
+          </Dialog>
+        </Paper>
+      )}
     </div>
   );
 }
